ef96437734244b878ca56c8f6d98d357969693d6
muhammad@muhammad-Inspiron-5521 <munib.rehman@nxb.com.pk>
2017-09-15 13:35:46 +0500

    updated api to integrate associated user

--- app/models/chat_participant.rb
+++ app/models/chat_participant.rb
@@ -5,9 +5,9 @@ class ChatParticipant < ActiveRecord::Base
 
 
   def self.conversation_exists(chatable_type, chatable_ids)
-    chatable_ids << user
-    count = chatable_ids.tr('[]', '').split(',').map(&:to_i).count
-    non_array_ids = chatable_ids.delete "[]"
+    chatable_ids << person.user
+    count = chatable_ids.count
+    non_array_ids = chatable_ids
     result = ActiveRecord::Base.connection.execute("select conversation_id from chat_participants where chatable_type = '#{chatable_type}' group by conversation_id
                having count(*) = #{count} and count(case when chatable_id not in (#{non_array_ids}) then 1 end) = 0").first
 
