35e4988c700ef3c749dfdc5c7bfb2d2283dd8c2e
munib@NX00585 <munib.rehman@nxb.com.pk>
2017-11-28 11:52:08 +0500

    staging api communicated with chatserver

--- app/models/device.rb
+++ app/models/device.rb
@@ -9,7 +9,7 @@ class Device < ActiveRecord::Base
     conversation = Conversation.find_by_id(message.conversation_id)
     chat_participants = conversation.chat_participants
     group_id = chat_participants&.first&.chatable_id if chat_participants.exists?(chatable_type:'Group')
-    url = URI.parse("http://10.28.83.53:3000/api/v2/push_notifications?app_id=#{national.app_id}&message=#{message.content}&group_id=#{group_id}&sender_id=#{message.sender_id}&conversation_id=#{message.conversation_id}")
+    url = URI.parse("http://staging-unionconnect.herokuapp.com/api/v2/push_notifications?app_id=#{national.app_id}&message=#{message.content}&group_id=#{group_id}&sender_id=#{message.sender_id}&conversation_id=#{message.conversation_id}")
     req = Net::HTTP::Get.new(url.to_s)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
